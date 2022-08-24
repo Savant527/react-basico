29e5c3d74d92c1f936cc41ce1207773a1eb39fa7
Savant527 <jhonsebastiannavas@gmail.com>
2022-08-23 21:37:39 -0500

    hasta Ciclo de vida

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Componente from './components/Componentes';
+import Propiedades from './components/Propiedades';
+import Estado from "./components/Estado";
+import RenderizadoCondicional from "./components/RenderizadoCondicional";
+import RenderizadoElementos from "./components/RenderizadoElementos";
+import { EventosES6, EventosES7, MasSobreEventos } from "./components/Eventos";
+import ComunicacionComponentes from "./components/ComunicacionComponentes";
+import CicloVida from './components/CicloVida';
 
 function App() {
   return (
@@ -18,6 +26,38 @@ function App() {
           Learn React
         </a>
       </header>
+      <Componente msg="Hola soy un componente funcional expresado desde una prop" />
+      <hr />
+      <Propiedades
+        cadena="Esto es una cadena de texto"
+        numero={19}
+        booleano={true}
+        arreglo={[1, 2, 3]}
+        objeto={{ nombre: "Jon", correo: "jonmircha@gmail.com" }}
+        funcion={(num) => num * num}
+        elementoReact={<i>Esto es un elemento React</i>}
+        componenteReact={
+          <Componente msg="Soy un componente pasado como Prop" />
+        }
+      />
+      <hr />
+      <Estado />
+      <hr />
+      <RenderizadoCondicional />
+      <hr />
+      <RenderizadoElementos />
+      <hr />
+      <EventosES6 />
+      <hr />
+      <EventosES7 />
+      <hr />
+      <MasSobreEventos />
+      <hr />
+      <ComunicacionComponentes />
+      <hr />
+      <CicloVida/>
+      <hr />
+      <hr />
     </div>
   );
 }
